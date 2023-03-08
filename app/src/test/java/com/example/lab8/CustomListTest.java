97a7e5383095d0c931a827bf35f38071f4452343
Yuyang Zeng <yzeng8@ualberta.ca>
2023-03-07 17:59:33 -0700

    RED:ADD THE HASCITY TEST

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -30,6 +30,13 @@ public class CustomListTest {
         assertEquals(1,list.getCount());
         list.deleteCity(city);
         assertEquals(0,list.getCount());
+    }
+
+    @Test
+    public void testHasCity(){
+        CustomList list = new CustomList(null,citylist);
+        City city = new City ("Edmonton","AB");
+        assertEquals(true,list.hasCity(city));
 
     }
 
