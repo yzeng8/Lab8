246fa8499c77d114efec462ab402fa4c2781bda9
Yuyang Zeng <yzeng8@ualberta.ca>
2023-03-07 17:40:06 -0700

    yzeng8

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private ArrayList<City> citylist= new ArrayList<>();
 
+    @Test
+    public void testAddCity(){
+        CustomList list = new CustomList(null,citylist);
+        City city = new City ("Edmonton","AB");
+        list.addCity(city);
+        assertEquals(1,list.getCount());
+
+    }
 
 }
